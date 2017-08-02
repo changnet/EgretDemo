0a8336de1647d064d8dd33fc6b790be6d007ba28
changnet <940477049@qq.com>
2017-08-02 20:15:10 +0800

    add star and lock in mini map

--- src/ui/RoomPage.ts
+++ src/ui/RoomPage.ts
@@ -1,7 +1,11 @@
 class MiniMap extends egret.DisplayObjectContainer {
+    private _lock: egret.Bitmap;
+    private _width: number;
+    private _star: egret.Bitmap[];
 
     constructor(conf: any) {
         super();
+        this._star = [];
 
         // 背景
         var bgImage = new egret.Bitmap();
@@ -14,6 +18,41 @@ class MiniMap extends egret.DisplayObjectContainer {
         var frame = new egret.Bitmap();
         frame.texture = RES.getRes("ui/ui.json#bk");
         this.addChild(frame);
+
+        this._width = bgImage.width;
+
+        this.lock = true;
+        this.star = 4;
+    }
+
+    public set lock(value: boolean) {
+        if (!this._lock) {
+            this._lock = new egret.Bitmap();
+            this._lock.texture = RES.getRes("ui/ui.json#suo")
+            this._lock.x = this.width * 0.5 - this._lock.width * 0.5
+            this._lock.y = this.height * 0.5 - this._lock.height * 0.5
+
+            this.addChild(this._lock)
+        }
+        this._lock.visible = value;
+    }
+
+    public set star(value: number) {
+        // 创建足够的星星
+        for ( var idx = this._star.length;idx < value;idx ++ ) {
+            let oneStar = new egret.Bitmap();
+            oneStar.texture = RES.getRes("ui/ui.json#star")
+
+            // 设置绝对锚点X。坐标x = oneStar.x - oneStar.anchorOffsetX
+            //oneStar.anchorOffsetX = oneStar.width / 2;
+
+            // 每颗星之前相隔2像素
+            oneStar.x = 25 + idx * (oneStar.width + 2);
+            oneStar.y = 0
+
+            this._star.push(oneStar);
+            this.addChild(oneStar);
+        }
     }
 }
 
